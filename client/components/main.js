b3cbf182722cf5b8bfe818e6192d4f40355adad3
Doug Chin <dougmchin@gmail.com>
2017-11-09 14:56:13 -0500

    veist is up

--- client/components/main.js
+++ client/components/main.js
@@ -1,1 +1,7 @@
 // main component is primary window, always will display navbar and dynamic character/vault gear sections
+
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {withRouter, Link} from 'react-router-dom'
---
+++ server/index.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const compression = require('compression')
+// const session = require('express-session')
+// const passport = require('passport')
+// const SequelizeStore = require('connect-session-sequelize')(session.Store)
+// const db = require('./db')
+// const sessionStore = new SequelizeStore({ db })
+const PORT = process.env.PORT || 4444
+const app = express()
+const socketio = require('socket.io')
+module.exports = app
+
+if (process.env.NODE_ENV !== 'production') require('../secrets')
+
+// body-parser
+// morgan log
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+.use((req, res, next) => {
+	if (path.extname(req.path).length) {
+		const err = new Error('Not found')
+		err.status = 404
+		next(err)
+		return null
+	} else {
+		next()
+		return null
+	}
+})
+
+app.use('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+app.use((err, req, res, next) => {
+	console.error(err)
+	console.error(err.stack)
+	res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+app.listen(PORT, () => {
+	console.log(`Being veisty on port ${PORT}`)
+})
