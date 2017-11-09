d8f0eeb4d0b561166049c390af6aaad8c99a1f01
Doug Chin <dougmchin@gmail.com>
2017-11-09 12:04:10 -0500

    todos

---
+++ todos.js
@@ -0,0 +1,17 @@
+// initial load-up: sign-in page to b.net account
+
+// one bnet account can be tied to psn and xbl
+// do we need sony api, or does bnet api take care of that?
+
+// once signed-up, most recent/powerful character is displayed
+// equipped gear is displayed
+// button toggles to show in-pocket gear for those slots
+// auto loadout selection. toggle gear with a lock status, if locked, gear won't be moved to vault. can auto move items up to limit. loadout labelled items in vault(or other characters) will be obtained
+// multi grab for gear, if there is room
+// complete flush to vault, respects lock status, and if not enough room moves weakest to vault
+// button that toggles vault sliding in or away
+//
+
+// dps based on impact, rpm
+// optimal firing range, based on range and stability
+// recommendation loadout, associations with other weapons based on desired activity
